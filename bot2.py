907671623fba0136f2a0e77d322c071948d5a280
negm1212 <168230397+negm1212@users.noreply.github.com>
2024-06-02 05:27:20 +0300

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -321,7 +321,7 @@ def menu_callback(call):
 						ccnn+=1
 					else:
 						dd += 1
-					time.sleep(20)
+					time.sleep(30)
 		except Exception as e:
 			print(e)
 		stopuser[f'{id}']['status'] = 'start'
@@ -433,7 +433,7 @@ def menu_callback(call):
 						ccnn+=1
 					else:
 						dd += 1
-					time.sleep(20)
+					time.sleep(30)
 		except Exception as e:
 			print(e)
 		stopuser[f'{id}']['status'] = 'start'
